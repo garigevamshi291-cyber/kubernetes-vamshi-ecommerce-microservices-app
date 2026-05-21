89c90220d8e0a4d610ad9ca04c270cd737ef4174
garigevamshi291-cyber <garigevamshi291@gmail.com>
2026-05-21 23:28:54 +0530

    Update main.tf

--- EKS-Terraform/main.tf
+++ EKS-Terraform/main.tf
@@ -296,7 +296,7 @@ resource "aws_eks_node_group" "node_group" {
 
 
 resource "aws_instance" "eks" {
-    ami           = "ami-02dfbd4ff395f2a1b"
+    ami           = "ami-00a9f44477dd83e3d"
     instance_type = "c7i-flex.large"
     subnet_id     = aws_subnet.public1.id
     vpc_security_group_ids = [aws_security_group.allow_all.id]
